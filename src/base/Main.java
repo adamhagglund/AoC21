0c49d53a76193dc798db685b24c4e88f63abea29
Anja Pettersson <anjapetterssonn@gmail.com>
2022-06-08 13:40:39 +0200

    initial commit for day5

--- src/base/Main.java
+++ src/base/Main.java
@@ -9,7 +9,7 @@ public class Main {
         //int[] inputx = parser.parseAllLinesAsInt("3");
         String[] input = parser.parseAllLinesAsString("3");
         //long[] input = parser.parseAllLinesAsLong("3");
-        Day3 day = new Day3(input);
+        Day5 day = new Day5(input);
 
         //System.out.println(day.solvePart1());
         System.out.println(day.solvePart2());
---
+++ src/days/Day5.java
@@ -0,0 +1,17 @@
+package days;
+
+public class Day5 {
+    int[] entries;
+
+    public Day5(int[] entries){
+        this.entries = entries;
+    }
+
+    public int solvePart1() {
+        return 0;
+    }
+
+    public int solvePart2(){
+        return 0;
+    }
+}
