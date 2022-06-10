9d660f9003f5c185dca7ab415ae7ea78f5b889bb
Anja Pettersson <anjapetterssonn@gmail.com>
2022-06-10 12:33:52 +0200

    day 2 is completed

--- src/base/Main.java
+++ src/base/Main.java
@@ -1,16 +1,16 @@
 package base;
 import base.Parser;
-import days.Day2ap;
+import days.Day4ap;
 
 public class Main {
 
     public static void main(String[] args) {
         Parser parser = new Parser();
         //int[] inputx = parser.parseAllLinesAsInt("day2");
-        String[] input = parser.parseAllLinesAsString("day2");
+        String[] input = parser.parseAllLinesAsString("day4");
         //long[] input = parser.parseAllLinesAsLong("3");
-        Day2ap day = new Day2ap(input);
-        System.out.println(day.solvePart2());
+        Day4ap day = new Day4ap(input);
+        System.out.println(day.solvePart1());
 
         //System.out.println(day.solvePart1());
         //System.out.println(day.solvePart2());
---
+++ src/days/Day4ap.java
@@ -0,0 +1,27 @@
+package days;
+
+public class Day4ap {
+    String[] entries;
+    int draw = 0;
+    boolean win;
+
+    public Day4ap(String[] entries){
+        this.entries = entries;
+    }
+
+    public int solvePart1() {
+        //skapa en ny vektor för alla drag
+        String s = entries[0];
+        String[] drawn = s.split(",");
+        System.out.println(drawn.toString());
+
+
+
+        return 0;
+    }
+
+
+    public int solvePart2(){
+        return 0;
+    }
+}
